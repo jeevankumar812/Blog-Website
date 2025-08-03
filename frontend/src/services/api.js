0608436356deaf6a456df9f586be56a9e4a31c2b
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-03 17:48:15 +0530

    added url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
+// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ Ensure this matches your backend
+  baseURL: "https://blog-website-backend-wc4k.onrender.com/api", // ✅ updated
   withCredentials: false,
 });
 
